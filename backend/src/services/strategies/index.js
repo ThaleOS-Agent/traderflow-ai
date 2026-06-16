c8e5395994eae2988d00b6e6f70d86a8d638ea1c
ThaleOS Network <223404713+ThaleOS-Agent@users.noreply.github.com>
2026-06-16 13:00:27 +1200

    Use explicit strategy dispatch

--- backend/src/services/strategies/index.js
+++ backend/src/services/strategies/index.js
@@ -5,31 +5,47 @@ import { MetalsTraderStrategy } from './metalsTrader.js';
 import { OilTraderStrategy } from './oilTrader.js';
 import { XQTradeM8Strategy } from './xqTradeM8.js';
 
-export const strategies = {
+export const strategies = Object.freeze({
   quantum_ai: QuantumAIStrategy,
   crypto_bot: CryptoBotStrategy,
   forex_pro: ForexProStrategy,
   metals_trader: MetalsTraderStrategy,
   oil_trader: OilTraderStrategy,
   xq_trade_m8: XQTradeM8Strategy
-};
+});
+
+const STRATEGY_CODES = Object.freeze([
+  'quantum_ai',
+  'crypto_bot',
+  'forex_pro',
+  'metals_trader',
+  'oil_trader',
+  'xq_trade_m8'
+]);
 
 export function getStrategy(name) {
-  const hasValidStrategy = Object.prototype.hasOwnProperty.call(strategies, name);
-  const Strategy = hasValidStrategy ? strategies[name] : strategies.xq_trade_m8;
-
-  if (typeof Strategy !== 'function') {
-    return new strategies.xq_trade_m8();
+  switch (name) {
+    case 'quantum_ai':
+      return new QuantumAIStrategy();
+    case 'crypto_bot':
+      return new CryptoBotStrategy();
+    case 'forex_pro':
+      return new ForexProStrategy();
+    case 'metals_trader':
+      return new MetalsTraderStrategy();
+    case 'oil_trader':
+      return new OilTraderStrategy();
+    case 'xq_trade_m8':
+    default:
+      return new XQTradeM8Strategy();
   }
-
-  return new Strategy();
 }
 
 export function getAllStrategies() {
-  return Object.keys(strategies).map(key => {
-    const strategy = new strategies[key]();
+  return STRATEGY_CODES.map(code => {
+    const strategy = getStrategy(code);
     return {
-      code: key,
+      code,
       name: strategy.name,
       description: strategy.description,
       type: strategy.type,
